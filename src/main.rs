033ac288c68fe53b28303f155f79100b49c5952d
Élise <elise@eiss.fr>
2023-10-16 13:16:54 +0200

    now taking glob as first argument

--- src/main.rs
+++ src/main.rs
@@ -28,13 +28,15 @@ fn process_entry(entry: &DirEntry, glob: &GlobMatcher) {
 }
 
 fn main() {
-    let arg = std::env::args().nth(1).unwrap_or(".".to_owned());
-    println!("{arg}");
-    let walker = WalkDir::new(arg)
+    let mut args = std::env::args().skip(1);
+    let filter = args.nth(0).expect("need a filter");
+    let root_dir_path = args.skip(1).nth(0).unwrap_or(".".to_owned());
+    //let arg = std::env::args().nth(1).unwrap_or(".".to_owned());
+    let walker = WalkDir::new(root_dir_path)
         .into_iter()
         .filter_entry(|e| !is_hidden(e));
     //let glob = Glob::new("*.rs").unwrap().compile_matcher();
-    let glob = GlobBuilder::new("**/*.rs")
+    let glob = GlobBuilder::new(&filter)
         .literal_separator(true)
         .build()
         .unwrap()

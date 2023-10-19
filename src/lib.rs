7b4aa572b5697c8ad1507b02e58daad412a50e6d
Élise <elise@eiss.fr>
2023-10-19 15:55:02 +0200

    moved logic to lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,29 @@
 pub mod path;
+
+pub fn flatten<I>(source_dir: &str, destination_dir: &str, files: I)
+where
+    I: Iterator<Item = String>,
+{
+    let source_dir = std::path::Path::new(source_dir);
+    let destination_dir = std::path::Path::new(destination_dir);
+    for file_path_str in files {
+        process_one(file_path_str, source_dir, destination_dir);
+    }
+}
+
+fn process_one(file_path_str: String, root: &std::path::Path, output_dir: &std::path::Path) {
+    let file_path = std::path::Path::new(&file_path_str);
+    let dest = path::gen_destination2(file_path, root, output_dir).unwrap();
+    create_dir_for(&dest).unwrap();
+    match std::fs::copy(file_path, &dest) {
+        Ok(_) => println!("{:?} -> {:?}", file_path, dest),
+        Err(error) => eprintln!("during copy {:?}", error),
+    }
+}
+
+fn create_dir_for(file_path: &std::path::Path) -> std::io::Result<()> {
+    if let Some(parent) = file_path.parent() {
+        std::fs::create_dir_all(parent)?;
+    }
+    return Ok(());
+}
---
+++ src/main.rs
@@ -0,0 +1,8 @@
+use delivery_flatener::*;
+
+fn main() {
+    let mut args = std::env::args().skip(1);
+    let source_dir = args.nth(0).expect("need a source dir");
+    let destination_dir = args.nth(0).expect("need a destination dir");
+    flatten(&source_dir, &destination_dir, args);
+}

971cad96d202a2c28aa9f1ce928fe02752091d35
Élise <elise@eiss.fr>
2024-02-21 14:40:25 +0100

    Removed unused funcs

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,19 @@
 pub mod cli;
 pub mod path;
 
+/// Sig. of a function that can generate the destination path
+/// of a file.
 type GenDestFn = fn(
     file: &std::path::Path,
     root_dir: &std::path::Path,
     output_dir: &std::path::Path,
 ) -> Option<std::path::PathBuf>;
 
+/// Copies files to a destination subdir keeping all components of the
+/// original paths. They are inserted in the filenames and separated by dots.
+/// example:
+/// "root/tata/src/stacks/create.c" becomes
+/// "dest/tata/src.stacks.create.c"
 pub fn flatten<I>(source_dir: &std::ffi::OsStr, destination_dir: &std::ffi::OsStr, files: I)
 where
     I: Iterator<Item = std::ffi::OsString>,
@@ -14,7 +21,7 @@ where
     let source_dir = std::path::Path::new(source_dir);
     let destination_dir = std::path::Path::new(destination_dir);
     for file_path_str in files {
-        process_one_keep(
+        process_one_file(
             file_path_str,
             source_dir,
             destination_dir,
@@ -23,7 +30,27 @@ where
     }
 }
 
-fn process_one_keep(
+/// Copies files to a destination subdir without preserving path components
+/// example:
+/// "root/tata/src/stacks/create.c" becomes
+/// "dest/tata/create.c"
+pub fn flatten_discard<I>(source_dir: &std::ffi::OsStr, destination_dir: &std::ffi::OsStr, files: I)
+where
+    I: Iterator<Item = std::ffi::OsString>,
+{
+    let source_dir = std::path::Path::new(source_dir);
+    let destination_dir = std::path::Path::new(destination_dir);
+    for file_path_str in files {
+        process_one_file(
+            file_path_str,
+            source_dir,
+            destination_dir,
+            path::gen_destination,
+        );
+    }
+}
+
+fn process_one_file(
     file_path_str: std::ffi::OsString,
     root: &std::path::Path,
     output_dir: &std::path::Path,
@@ -49,40 +76,4 @@ fn create_dir_for(file_path: &std::path::Path) -> std::io::Result<()> {
         std::fs::create_dir_all(parent)?;
     }
     return Ok(());
-}
-
-pub fn flatten_discard<I>(source_dir: &std::ffi::OsStr, destination_dir: &std::ffi::OsStr, files: I)
-where
-    I: Iterator<Item = std::ffi::OsString>,
-{
-    let source_dir = std::path::Path::new(source_dir);
-    let destination_dir = std::path::Path::new(destination_dir);
-    for file_path_str in files {
-        process_one_keep(
-            file_path_str,
-            source_dir,
-            destination_dir,
-            path::gen_destination,
-        );
-    }
-}
-
-fn process_one_discard(
-    file_path_str: std::ffi::OsString,
-    root: &std::path::Path,
-    output_dir: &std::path::Path,
-) {
-    let file_path = std::path::Path::new(&file_path_str);
-    let Some(dest) = path::gen_destination(file_path, root, output_dir) else {
-        eprintln!("path generation failed for {:?}", file_path_str);
-        return;
-    };
-    if let Err(err) = create_dir_for(&dest) {
-        eprintln!("{}", err);
-        return;
-    };
-    match std::fs::copy(file_path, &dest) {
-        Ok(_) => println!("{:?} -> {:?}", file_path, dest),
-        Err(error) => eprintln!("during copy {:?}", error),
-    }
 }
